cb24282b4b10d8ae1e4ea1b11fc18d49d2be7c76
Abraham youkhanna <abrahamy3322@gmail.com>
2020-11-14 12:29:43 -0600

    added hired in 1986 table

--- queries.sql
+++ queries.sql
@@ -1,5 +1,12 @@
+--employee number, last name, first name, sex, and salary--
 SELECT employees.emp_no, salary, first_name, last_name, sex 
 FROM salaries
 INNER JOIN employees 
 ON salaries.emp_no = employees.emp_no;
+
+--hire in 1986--
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date 
+BETWEEN '1986-01-01' and '1986-12-31';
 
