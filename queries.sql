3483cfea83d16b715544d3a6472491d545571217
Abraham youkhanna <abrahamy3322@gmail.com>
2020-11-14 12:36:37 -0600

    added department manager and employee department tables

--- queries.sql
+++ queries.sql
@@ -10,3 +10,24 @@ FROM employees
 WHERE hire_date 
 BETWEEN '1986-01-01' and '1986-12-31';
 
+--Department Manager--
+SELECT
+employees.first_name,
+employees.last_name,
+department_manager.emp_no,
+departments.dept_no,
+departments.dept_name
+FROM employees
+JOIN department_manager on employees.emp_no=department_manager.emp_no
+JOIN departments on departments.dept_no=department_manager.dept_no;
+
+--department of each employee--
+SELECT
+employees.first_name,
+employees.last_name,
+employees.emp_no,
+departments.dept_name
+FROM employees
+JOIN department_employee on employees.emp_no=department_employee.emp_no
+JOIN departments on departments.dept_no=department_employee.dept_no;
+
